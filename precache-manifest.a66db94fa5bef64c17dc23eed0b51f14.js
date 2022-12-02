57cf06ef674a6c10f204acd064f5be2d81b48789
Tino <darkxeno@gmail.com>
2022-12-03 00:20:48 +0100

    Updates

---
+++ precache-manifest.a66db94fa5bef64c17dc23eed0b51f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce970179cab78ce929a5",
+    "url": "/cft-cv/static/js/main.ce970179.chunk.js"
+  },
+  {
+    "revision": "40d5359b02aa3e0e8e17",
+    "url": "/cft-cv/static/css/1.c3b5f68f.chunk.css"
+  },
+  {
+    "revision": "40d5359b02aa3e0e8e17",
+    "url": "/cft-cv/static/js/1.40d5359b.chunk.js"
+  },
+  {
+    "revision": "3223893bd41a0cd7e012",
+    "url": "/cft-cv/static/js/runtime~main.3223893b.js"
+  },
+  {
+    "revision": "558b6fbe7d07f45892283fa5fea7ebaf",
+    "url": "/cft-cv/static/media/photo.558b6fbe.jpeg"
+  },
+  {
+    "revision": "60b933664c0ef4f892bb7216967fadf8",
+    "url": "/cft-cv/index.html"
+  }
+];
