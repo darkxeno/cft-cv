11f1cbc631763225670eca801716ffc148bcecf2
Tino <darkxeno@gmail.com>
2022-12-02 23:54:03 +0100

    Dec 2022 update

--- src/presentation.js
+++ src/presentation.js
@@ -9,10 +9,6 @@ import {
   Text,
   Image,
 } from 'spectacle';
-import Terminal from 'spectacle-terminal';
-
-// Import theme
-import { mergeTheme } from 'spectacle/lib/theme';
 
 // Require CSS
 require('normalize.css');
@@ -25,20 +21,33 @@ const theme = {
     quaternary: 'white',
   },
   fonts: {
-    primary: 'Open Sans Condensed',
-    secondary: 'Open Sans Condensed',
+    primary: 'Helvetiva Neue',
+    secondary: 'Helvetiva Neue',
   }
 };
 
 const textStyle1 = {
-  fontSize:'1.25em',
+  fontSize:'1.5em',
   lineHeight:'1.5em',
   textAlign:'left',
-  margin: 0,
   padding: 0,
   margin: '0 1em',
   color: 'white',
 };
+
+const TextStyle1 = ({children}) => <Text style={textStyle1}>{children}</Text>
+
+const GreySpan = ({children}) => <span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>{children}</span>
+
+const Heading25 = ({children}) => <Heading fontSize="25px" backgroundColor="white" style={{borderRadius: '10px'}} >{children}</Heading>
+
+const MonoText =  ({children}) => <Text style={{ ...textStyle1, fontSize: '1.5em', lineHeight: '1em', fontFamily: 'monospace', color: "white", margin: '0px 24px'  }}>{children}</Text>
+
+const LabelDiv = ({children}) => <div style={{color: 'lightgrey', marginTop: '1rem'}}>{children}</div>
+
+const TechDiv = ({children}) => <div style={{marginLeft: '2rem', marginTop: '0.25rem'}}>{children}</div>
+
+const Expert = () => <span style={{color: '#33B969'}}>&#9733;</span>
 
 export default class Presentation extends React.Component {
   render() {
@@ -47,12 +56,13 @@ export default class Presentation extends React.Component {
         theme={theme}
       >
         <Slide backgroundColor="secondary">
-          <Heading size={4} caps lineHeight={1} backgroundColor="quaternary">
-            Constantino Fernandez Traba            
-          </Heading>
-          <Heading size={6} caps lineHeight={1} backgroundColor="quaternary">
+          <Heading25>
             CURRICULUM VITAE
-          </Heading>
+          </Heading25>
+          <Image src={require('./photo.jpeg')} width="200px" style={{borderRadius: '10px',alignSelf: 'center'}} />
+          <Heading25>
+            Constantino Fernández Traba            
+          </Heading25>          
           <br/>          
           <a
             href="https://es.linkedin.com/in/constantinofernandeztraba/en"
@@ -77,122 +87,107 @@ export default class Presentation extends React.Component {
           <br/>
           <br/>
           <br/>
-          <Text style={{...textStyle1, fontSize: '0.75em', textAlign: 'right'}}>Last update: 5th September 2021</Text>
+          <Text style={{...textStyle1, fontSize: '0.75em', textAlign: 'right'}}>Last update: 2nd December 2022</Text>
         </Slide>
-        <Slide backgroundColor="primary" textColor="white" textColor="secondary">
-          <Heading size={6} backgroundColor="white">
+        <Slide backgroundColor="primary" textColor="secondary">
+          <Heading25>
           CURRENT POSITION
-          </Heading>
-          <Text style={textStyle1} color="quaternary">
-          DevOps Lead / Engineering Manager<span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}> Mobgen / AI [Jan 2021 - now]</span>
-          </Text><br/>
-          <Heading size={6} backgroundColor="white">
+          </Heading25>          
+          <TextStyle1>Senior DevOps Engineer<GreySpan> Artbanx [Feb 2022 - now]</GreySpan></TextStyle1>
+          <br/>
+          <Heading25>
           EXPERIENCE / ROLES
-          </Heading>    
-          <Text style={textStyle1} color="quaternary">DevOps Lead / Engineering Manager<span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}> Mobgen / AI [Jan 2021 - now]</span></Text>
-          <Text style={textStyle1} color="quaternary">Solution Architect / DevOps (multiple projects)<span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}> Mobgen / AI [Mar 2019 - Dec 2020]</span></Text>
-          <Text style={textStyle1} color="quaternary">Product Manager / Information Security Lead - Halo<span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Mobgen / AI  [Mar 2017 - Dec 2019]</span></Text>
-          <Text style={textStyle1} color="quaternary">Solution Architect - Halo<span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Mobgen [Dec 2015 - Jan 2018]</span></Text>
-          <Text style={textStyle1} color="quaternary">Lead Full Stack Web Engineer <span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Mobgen [Feb 2014 - Dec 2015]</span></Text>
-          <Text style={textStyle1} color="quaternary">Cofounder and Collaborator <span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Codefixia [Sep 2012 - Sep 2014]</span></Text>
-          <Text style={textStyle1} color="quaternary">Senior Web / Mobile Developer <span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Restalo [Jan 2011 - Feb 2014]</span></Text>
-          <Text style={textStyle1} color="quaternary">Web/Mobile Developer <span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Wombat Internet [Mar 2010 - Jan 2011]</span></Text>
-          <Text style={textStyle1} color="quaternary">Deployment Manager <span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Lemonquest [Feb 2009 - Feb 2010]</span></Text>
-          <Text style={textStyle1} color="quaternary">Mobile Games Developer <span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Lemonquest [Sep 2008 - Sep 2009]</span></Text>
-          <Text style={textStyle1} color="quaternary">Web Developer <span style={{color: 'lightgrey',fontSize:'1.5rem',float:'right'}}>Redegal [Jul 2006 - Sep 2006]</span></Text>
+          </Heading25>              
+          <TextStyle1>DevOps Lead / Engineering Manager<GreySpan>Mobgen / AI [Jan 2021 - Feb 2022]</GreySpan></TextStyle1>
+          <TextStyle1>Solution Architect / DevOps<GreySpan> Mobgen / AI [Mar 2019 - Dec 2020]</GreySpan></TextStyle1>
+          <TextStyle1>Product Manager / Information Security Lead - Halo<GreySpan>Mobgen / AI  [Mar 2017 - Dec 2019]</GreySpan></TextStyle1>
+          <TextStyle1>Solution Architect - Halo<GreySpan>Mobgen [Dec 2015 - Jan 2018]</GreySpan></TextStyle1>
+          <TextStyle1>Lead Full Stack Web Engineer <GreySpan>Mobgen [Feb 2014 - Dec 2015]</GreySpan></TextStyle1>
+          <TextStyle1>Cofounder and Collaborator <GreySpan>Codefixia [Sep 2012 - Sep 2014]</GreySpan></TextStyle1>
+          <TextStyle1>Senior Web / Mobile Developer <GreySpan>Restalo [Jan 2011 - Feb 2014]</GreySpan></TextStyle1>
+          <TextStyle1>Web/Mobile Developer <GreySpan>Wombat Internet [Mar 2010 - Jan 2011]</GreySpan></TextStyle1>
+          <TextStyle1>Deployment Manager <GreySpan>Lemonquest [Feb 2009 - Feb 2010]</GreySpan></TextStyle1>
+          <TextStyle1>Mobile Games Developer <GreySpan>Lemonquest [Sep 2008 - Sep 2009]</GreySpan></TextStyle1>
+          <TextStyle1>Web Developer <GreySpan>Redegal [Jul 2006 - Sep 2006]</GreySpan></TextStyle1>
         </Slide>
         <Slide> 
-          <Heading size={6} backgroundColor="white">
+          <Heading25>
             TECHNOLOGIES / TOOLS
-          </Heading>  
-          <Text style={{ ...textStyle1, fontSize: '1.5em', lineHeight: '1em', fontFamily: 'monospace', color: "white",  }}>                          
-            <br/><br/>
-            <span style={{color: 'lightgrey'}}>DevOps:</span>
-              &nbsp;kubernetes <span style={{color: '#33B969'}}>expert</span>, docker <span style={{color: '#33B969'}}>expert</span>, cloudformation, helm
-              CI/CD (Azure DevOps <span style={{color: '#33B969'}}>expert</span>, Bamboo, Drone), bash scripting, linux, ansible, ...
-            <br/><br/>
-            <span style={{color: 'lightgrey'}}>Monitoring / Observability:</span>
-              &nbsp;azure application insights <span style={{color: '#33B969'}}>expert</span>, new relic <span style={{color: '#33B969'}}>expert</span>, aws cloudwatch, prometheus, fluentd, elastic...
-            <br/><br/>                                    
-            <span style={{color: 'lightgrey'}}>AWS Cloud:</span>
-              &nbsp;ec2 <span style={{color: '#33B969'}}>expert</span>, ecs, eks, iam, vpc, cloudwatch, rds
-              dynamodb, documentdb, s3, cloudfront, API Gateway, lambda, acm, ...
-            <br/><br/> 
-            <span style={{color: 'lightgrey'}}>Azure Cloud:</span>
-              &nbsp;app service, functionApps, aks, app. insights <span style={{color: '#33B969'}}>expert</span>, keyVault, blob storage, ...
-            <br/><br/>                       
-            <span style={{color: 'lightgrey'}}>DBs/others:</span>
-              &nbsp;mysql <span style={{color: '#33B969'}}>expert</span>, mongodb <span style={{color: '#33B969'}}>expert</span>, kafka (streams), redis
-               memcached, varnish, cassandra, ...
-            <br/><br/>            
-            <span style={{color: 'lightgrey'}}>Backend:</span>
-              &nbsp;nodejs <span style={{color: '#33B969'}}>expert</span>, php, scala, java
-            <br/><br/>            
-            <span style={{color: 'lightgrey'}}>Frontend:</span>
-              &nbsp;javascript <span style={{color: '#33B969'}}>expert</span>, react <span style={{color: '#33B969'}}>expert</span>
-            <br/><br/>            
-            <span style={{color: 'lightgrey'}}>Machine Learning:</span>
-              &nbsp;tensorflow, keras, python, matlab (beginner)
-            <br/><br/>            
-            <span style={{color: 'lightgrey'}}>Mobile:</span>
-             &nbsp;iOS objective C, Android Java (old)
-            <br/><br/>
-          </Text>     
+          </Heading25>  
+          <MonoText>
+            <LabelDiv>Cloud:</LabelDiv>
+              <TechDiv>AWS<Expert/>, Azure<Expert/>, Google Cloud<Expert/></TechDiv>
+            <LabelDiv>DevOps:</LabelDiv>
+              <TechDiv>kubernetes<Expert/>, terraform<Expert/>, docker<Expert/>, helm<Expert/>, cloudformation, 
+              bash, ansible</TechDiv>            
+            <LabelDiv>CI/CD:</LabelDiv>              
+              <TechDiv>Azure DevOps<Expert/>, Github Actions / Workflows<Expert/>, argocd (gitops), Google Cloud Build, AWS CodeBuild, Bamboo, Drone</TechDiv>                      
+            <LabelDiv>Monitoring / Observability:</LabelDiv>
+              <TechDiv>azure application insights<Expert/>, newrelic<Expert/>, opentelemetry<Expert/>, prometheus, grafana, aws cloudwatch, fluentd, elastic</TechDiv>                                                                      
+            <LabelDiv>DBs/others:</LabelDiv>
+              <TechDiv>mysql<Expert/>, mongodb<Expert/>, kafka (streams), redis,
+               memcached, varnish, cassandra, postgresql</TechDiv>            
+            <LabelDiv>Backend:</LabelDiv>
+              <TechDiv>nodejs<Expert/>, scala, java</TechDiv>            
+            <LabelDiv>Frontend:</LabelDiv>
+              <TechDiv>javascript<Expert/>, react<Expert/></TechDiv>
+            <LabelDiv>Soft Skills:</LabelDiv>
+              <TechDiv>team player, leading by example, problem solver, ideator, learner, optimistic, motivator, out of the box thinking</TechDiv>            
+          </MonoText>     
         </Slide>
         <Slide backgroundColor="primary" textColor="quaternary"> 
-          <Heading size={6} caps lineHeight={1} backgroundColor="white">
+          <Heading25>
             COURSES / CERTIFICATIONS
-          </Heading>
-            <br/>
+          </Heading25>
             <a href="https://www.credly.com/badges/7d442462-a82c-46ec-b2fc-1d4e68e53857/linked_in" target="blank">
-              <Text style={textStyle1}>Certified SAFe  5 Agilist (2021)</Text>
+              <TextStyle1>Certified SAFe  5 Agilist (2021)</TextStyle1>
             </a>            
             <a href="https://management30.com/badges/constantino-fernandez-traba-1712822620601bdf7d8a081/" target="blank">
-              <Text style={textStyle1}>Management 3.0 - Fundamentals Online Workshop (2021)</Text>  
+              <TextStyle1>Management 3.0 - Fundamentals Online Workshop (2021)</TextStyle1>
             </a>             
             <a href="https://www.linkedin.com/school/salesbusinessschool/" target="blank">
-              <Text style={textStyle1}>Master in Sales - Sales Business School (2020)</Text>  
+              <TextStyle1>Master in Sales - Sales Business School (2020)</TextStyle1>
             </a>            
             <a href="https://www.scrum.org/user/400220" target="blank">
-              <Text style={textStyle1}>
+              <TextStyle1>
               Professional Scrum Product Owner<br/>
               Professional Scrum Master
-              </Text> 
+              </TextStyle1> 
             </a>            
             <a href="https://www.coursera.org/account/accomplishments/specialization/7697MXER3CNN" target="blank">
-              <Text style={textStyle1}>Cloud Computing Specialization</Text>  
+              <TextStyle1>Cloud Computing Specialization</TextStyle1>
             </a>
             <a href="https://www.coursera.org/account/accomplishments/verify/EXPTYF6YSLUL" target="blank">
-              <Text style={textStyle1}>Neural Networks and Deep Learning</Text> 
+              <TextStyle1>Neural Networks and Deep Learning</TextStyle1>
             </a>              
             <a href="https://www.coursera.org/account/accomplishments/verify/L2B8H6Q3W5X7" target="blank">
-              <Text style={textStyle1}>Improving Deep Neural Networks</Text> 
+              <TextStyle1>Improving Deep Neural Networks</TextStyle1>
             </a>
             <a href="https://www.coursera.org/learn/machine-learning" target="blank">
-              <Text style={textStyle1}>Machine Learning</Text> 
+              <TextStyle1>Machine Learning</TextStyle1> 
             </a> 
-            <Text style={textStyle1}>AWS Solution Architect</Text> 
-            <Text style={textStyle1}>Functional Programming Principles in Scala by Martin Odersky</Text> 
-            <Text style={textStyle1}>Creative Programming for Digital Media & Mobile Apps University of London</Text> 
-            <Text style={textStyle1}>CAD Design University of Vigo</Text> 
-            <Text style={textStyle1}>Documentaries Production University of Vigo</Text> 
-            <Text style={textStyle1}>Selling Techniques - Commerce Camera</Text>             
+            <TextStyle1>AWS Solution Architect</TextStyle1> 
+            <TextStyle1>Functional Programming Principles in Scala by Martin Odersky</TextStyle1> 
+            <TextStyle1>Creative Programming for Digital Media & Mobile Apps University of London</TextStyle1> 
+            <TextStyle1>CAD Design University of Vigo</TextStyle1> 
+            <TextStyle1>Documentaries Production University of Vigo</TextStyle1> 
+            <TextStyle1>Selling Techniques - Commerce Camera</TextStyle1>
           </Slide>    
         <Slide>                           
-          <Heading size={6} caps lineHeight={1} backgroundColor="white">
+          <Heading25>
             EDUCATION
-          </Heading>
-          <Text style={textStyle1} color="quaternary">
-            CAREER: TECHNICAL ENGINEER CS<br/>
-            University Of Vigo
-          </Text><br/>
-          <Heading size={6} caps lineHeight={1} backgroundColor="white">
+          </Heading25>
+          <TextStyle1>
+            Career: TECHNICAL ENGINEER COMPUTER SCIENCE<br/>
+            @ University Of Vigo
+          </TextStyle1><br/>
+          <Heading25>
             HONORARY DEGREES
-          </Heading><br/>
-          <Text style={textStyle1} color="quaternary">
+          </Heading25>
+          <TextStyle1>
             Multimedia Systems Flash/ActionScript<br/>
-            Computer Graphics OpenGL<br/>
-          </Text>
+            Computer Graphics OpenGL
+          </TextStyle1>
         </Slide>        
       </Deck>
     );
